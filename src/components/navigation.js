b19e3c8a7b8faf879edaaded820590058eeef74d
Anastasia Proskuryakova <anproskuryakova@ozon.ru>
2023-06-02 21:57:39 +0600

    made menu looks and about

---
+++ src/components/navigation.js
@@ -0,0 +1,23 @@
+import { anchorSubscribe,anchorAbout,anchorLooks} from "./constants";
+
+
+function scrollToAnchor(anchors) {
+  anchors.forEach(function (anchor) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const sectionID = anchor.getAttribute('href');
+      menuIcon.classList.remove('header__menu-pic_type_close');
+      menu.classList.add('menu_hidden');
+      document.querySelector(sectionID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  });
+}
+
+export function scrollToAnchors() {
+  scrollToAnchor(anchorSubscribe);
+  scrollToAnchor(anchorAbout)
+  scrollToAnchor(anchorLooks)
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlReplaceWebpackPlugin = require('html-replace-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const imageminPngquant = require('imagemin-pngquant');
+const imageminJpegtran = require('imagemin-jpegtran');
 
 module.exports = {
-  entry: {
-    main: './src/components/index.js'
-  },
+  entry: { main: './src/components/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -16,33 +17,28 @@ module.exports = {
   mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
-    open: true,
     compress: true,
-    port: 8080
+    port: 8080,
+    open: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: 'babel-loader',
-        exclude: '/node_modules/'
+        exclude: '/node_modules/',
       },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
       },
       {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
-          },
-          'postcss-loader',
-        ],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
@@ -50,13 +46,29 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
+    new HtmlReplaceWebpackPlugin([
+      {
+        pattern: /media="\(device-width:\s\d+px\)\sand\s\(device-height:\s\d+px\)\sand\s\(-webkit-device-pixel-ratio:\s\d\)\sand\s\(orientation:\s\w+\)"/g,
+        replacement: '',
+      },
+    ]),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/images'),
+          to: 'images/[name].[ext]',
+          globOptions: {
+            ignore: ['**/index.js'],
+          },
+        },
+      ],
+    }),
+    new ImageminPlugin({
+      plugins: [
+        imageminPngquant(),
+        imageminJpegtran(),
+      ],
+    }),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-    ],
-  },
 };
