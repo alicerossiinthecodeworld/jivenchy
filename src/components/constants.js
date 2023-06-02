48004834190d04feba3797d300df34018847bae7
Anastasia Proskuryakova <anproskuryakova@ozon.ru>
2023-06-02 18:54:03 +0600

    menu, about, looks made

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,17 @@
 export const menu = document.querySelector('.menu');
 export const menuIcon = document.querySelector('.header__menu-pic');
+
+export const src = [
+  "https://wwd.com/wp-content/uploads/2021/01/Givenchy-S21-001.jpg",
+  "https://cms.slimimagazine.com/slimi/assets/dq4mdeinfls0oksg",
+  "https://n.nordstrommedia.com/id/1049f275-19ee-41a8-985e-6e616fb44157.jpeg?h=837&w=513",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToNIP9JIBKC8l4UOVFzXcGyiPoNxuuZw4F9g&usqp=CAU",
+  "https://images-prod.anothermag.com/1000/azure/another-prod/400/2/402383.jpg",
+  "https://n.nordstrommedia.com/id/5079876c-d725-4ca7-aa46-b11e168c5534.jpeg?h=831&w=513",
+  "https://qcegmag.com/images/fashion/givenchy/32-thumb.jpg",
+  "https://graziamagazine.com/us/wp-content/uploads/sites/15/2021/10/GettyImages-1235680422-1.jpg?fit=1280%2C721",
+  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.designscene.net%2Fwp-content%2Fuploads%2F2020%2F10%2FGivenchy-Spring-Summer-2021-52.jpg&tbnid=_7PReolPkpk0HM&vet=12ahUKEwiHwb2-wKT_AhWWvioKHQaZBCsQMygBegUIARC9AQ..i&imgrefurl=https%3A%2F%2Fwww.designscene.net%2F2020%2F10%2Fgivenchy-ss21-matthew-williams.html&docid=VIgPzJpDjwpPDM&w=730&h=1095&q=givenchy%20spring%20summer%202021%20mathew%20&ved=2ahUKEwiHwb2-wKT_AhWWvioKHQaZBCsQMygBegUIARC9AQ",
+  "https://www.google.com/imgres?imgurl=https%3A%2F%2Ffashionista.com%2F.image%2Ft_share%2FMTc1ODY5MDI3MjQ2Mjg1ODc5%2Fgivenchy_rtw_ss21_visuels_plein_format_a4_7.jpg&tbnid=4zxcXMmTRnYxhM&vet=12ahUKEwiHwb2-wKT_AhWWvioKHQaZBCsQMygIegUIARDLAQ..i&imgrefurl=https%3A%2F%2Ffashionista.com%2F2020%2F10%2Fgivenchy-spring-2021-review&docid=eEgxEkv_lA2XqM&w=848&h=1200&q=givenchy%20spring%20summer%202021%20mathew%20&ved=2ahUKEwiHwb2-wKT_AhWWvioKHQaZBCsQMygIegUIARDLAQ",
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.robbreport.com.sg%2Fgivenchy-spring-summer-2021-a-closer-look-at-matthew-m-williams-debut-collection-for-the-french-luxury-house%2F&psig=AOvVaw1IUznIH_23uUeDHY4fmvLf&ust=1685792475234000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCOjXhb_ApP8CFQAAAAAdAAAAABAR",
+  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.vogue.fr%2Fphotos%2F5f7ae5d357d1357eb01195fb%2Fmaster%2Fw_1600%252Cc_limit%2F00013-GIVENCHY-SS21-RTW.jpg&tbnid=S0tbsfimaLWB6M&vet=12ahUKEwiHwb2-wKT_AhWWvioKHQaZBCsQMygKegUIARDPAQ..i&imgrefurl=https%3A%2F%2Fwww.vogue.fr%2Ffashion%2Farticle%2Fgivenchy-matthew-m-williams-first-collection-spring-summer-2021&docid=gtNIuHeVLfCRDM&w=720&h=1080&q=givenchy%20spring%20summer%202021%20mathew%20&ved=2ahUKEwiHwb2-wKT_AhWWvioKHQaZBCsQMygKegUIARDPAQ"
+];
